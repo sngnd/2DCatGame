18302ba7f9dbb3d33621ef82f3564c0587b98044
BaranovaAV <baranova7465@gmail.com>
2021-03-14 23:53:25 +0300

    initial commit

---
+++ Assets/Scripts/PlayerBulletCtrl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBulletCtrl : MonoBehaviour {
+
+    public Vector2 velocity;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = velocity;
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            GameCtrl.instance.BulletHitEnemy(collision.gameObject.transform);
+            Destroy(gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
